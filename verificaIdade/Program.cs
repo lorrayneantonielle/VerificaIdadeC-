e38e5d38d94af8a47326943cdba9c4df4dc8fc7a
lsr.antonielle@gmail.com <lsr.antonielle@gail.com>
2021-09-03 15:47:17 -0300

    add files

---
+++ verificaIdade/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace verificaIdade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade =int.Parse(Console.ReadLine());
+
+          
+
+            if (idade >= 1 && idade <= 11)
+            {
+                Console.WriteLine("Criança.");
+            }else if (idade >= 12 && idade <= 18)
+            {
+                Console.WriteLine("Adolescente.");
+            }else if (idade >= 19 && idade <= 64)
+            {
+                Console.WriteLine("Adulto.");
+            }else
+            {
+                Console.WriteLine("Idoso.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
